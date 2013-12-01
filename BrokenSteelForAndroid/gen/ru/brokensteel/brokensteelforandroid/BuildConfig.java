a96cf02f70fdf6ee58f3236d4cc48e7e534e4b28
Waldemar <waldemar.yan@gmail.com>
2013-12-01 23:25:04 +0700

    add project in repo

---
+++ BrokenSteelForAndroid/gen/ru/brokensteel/brokensteelforandroid/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package ru.brokensteel.brokensteelforandroid;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
